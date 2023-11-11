c730ef19edbf4c35f8202c53b6a8756b28b97998
Nikolay Novikov <AnourValar@gmail.com>
2023-11-11 11:57:46 +0300

    patch

--- src/Repository.php
+++ src/Repository.php
@@ -29,20 +29,57 @@ abstract class Repository
     /**
      * Get SQL values: 'foo', 'bar', 'baz'
      *
-     * @param array $attributes
+     * @param array|string $attributes
      * @return string
      */
-    protected function getRawValues(array $attributes): string
+    protected function getRawValues(array|string $attributes): string
     {
         $result = '';
 
-        foreach ($attributes as $item) {
+        foreach ((array) $attributes as $item) {
             if ($result) {
                 $result .= ', ';
             }
 
             $result .= \DB::escape($item);
         }
+
+        return $result;
+    }
+
+    /**
+     * Merge data from differenct sources (queries)
+     *
+     * @param array $sources
+     * @param string $groupBy
+     * @return array
+     */
+    protected function mergeSources(array $sources, string $groupBy): array
+    {
+        $result = [];
+        $structure = [];
+
+        foreach ($sources as $source) {
+            foreach ($source as $item) {
+                foreach ($item as $itemKey => $itemValue) {
+                    if (! isset($result[$item->$groupBy])) {
+                        $result[$item->$groupBy] = new \stdClass();
+                    }
+
+                    $structure[$itemKey] = (is_integer($itemValue) || is_float($itemValue)) ? 0 : null;
+                    $result[$item->$groupBy]->$itemKey = $itemValue;
+                }
+            }
+        }
+
+        foreach ($result as &$item) {
+            foreach ($structure as $structureKey => $structureValue) {
+                if (! isset($item->$structureKey)) {
+                    $item->$structureKey = $structureValue;
+                }
+            }
+        }
+        unset($item);
 
         return $result;
     }
